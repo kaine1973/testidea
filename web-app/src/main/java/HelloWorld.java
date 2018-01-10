0e247fa0ba0df19d321863a7493106b23df9b83b
kaine1973 <kaidanalenko@outlook.com>
2018-01-10 11:25:04 +0800

    第一次上传

---
+++ web-app/src/main/java/HelloWorld.java
@@ -0,0 +1,22 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+@WebServlet("/hello")
+public class HelloWorld extends HttpServlet {
+
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+        response.setCharacterEncoding("GBK");
+        String abc = request.getParameter("abc");
+        abc = new String(abc.getBytes("iso-8859-1"),"GBK");
+
+        try {
+            response.getWriter().write(abc);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
